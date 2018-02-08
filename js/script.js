1c16631131a7076a11e92643804e3bd717aa9b7d
Bartłomiej Bajda <allerbbb@gmail.com>
2018-02-08 16:57:36 +0000

    second update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-//color Span
-$('span:even').css('color','red');
-// add Buttons
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
-  var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
-  $(element).append(button);
-});
-// onClick on Buttons
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
+$(function(){
+  //color Span
+  $('span:even').css('color','red');
+  // add Buttons
+  var paragraphs = $('p');
+  paragraphs.each(function(index, element) {
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+    $(element).append(button);
+  });
+  // onClick on Buttons
+  $("button").on('click', function() {
+  	alert($(this).attr("data-tmp"));
+  });
 });
